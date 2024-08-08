fd459491013c18a53137c818b8f1ce1f513a1476
Galangzz <galangpro280605@gmail.com>
2024-08-08 11:31:41 +0700

    Revisi 1

--- index.js
+++ index.js
@@ -1,50 +1,87 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     // Current Time
-//     function updateTime() {
-//         const now = new Date();
-//         const timeString = now.toLocaleTimeString();
-//         document.getElementById('current-time').textContent = timeString;
-//     }
-//     setInterval(updateTime, 1000);
-//     updateTime();
-
-
-//     // function formatTime(seconds) {
-//     //     const hrs = Math.floor(seconds / 3600);
-//     //     const mins = Math.floor((seconds % 3600) / 60);
-//     //     const secs = seconds % 60;
-//     //     // if(hrs < 10){
-//     //     //     hrs = "0" + Math.floor(seconds / 3600);
-//     //     // }
-//     //     return `${String(hrs).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
-//     // }
-// })
-
-
 //CLOCK
+// document.addEventListener('DOMContentLoaded', () => {
 var waktu = document.getElementById("current-time")
 
-function jamSekarang(){
-    var waktuSekarang = new Date()
+if (waktu) {
+    
+    function jamSekarang(){
+        var waktuSekarang = new Date()
     var jam = waktuSekarang.getHours();
     var menit = waktuSekarang.getMinutes();
     var detik = waktuSekarang.getSeconds();
     var formatWaktu ="AM";
-
+    
     formatWaktu = (jam>=12)? "PM" : "AM";
-
+    
     if(jam == 0){
         jam = 12;
     }else if(jam > 12){
         jam -= 12;
     }
-
+    
     jam = (jam < 10) ? "0" + jam : jam;
     menit= (menit < 10) ? "0" + menit : menit;
     detik = (detik < 10) ? "0" + detik : detik;
-
-    waktu.innerHTML = jam + ":" + menit + ":" + detik + " " + formatWaktu;
+    
+    waktu.innerHTML = `${jam}:${menit}:${detik} ${formatWaktu}`;
+    
 };
-
 setInterval(jamSekarang, 1000);
 jamSekarang();
+}
+// });
+
+
+
+//TIMER
+const display = document.getElementById('time-watch');
+if(display){
+    let timer = null;
+    let startTime = 0;
+    let elapsedTime = 0;
+    let isRunning = false;
+    
+    function start(){
+        if(!isRunning){
+            startTime = Date.now() - elapsedTime;
+            timer = setInterval(update, 10)
+            isRunning = true;
+        }
+    }
+    
+    function stop(){
+        if(isRunning){
+            clearInterval(timer);
+            elapsedTime = Date.now() - startTime;
+            isRunning = false;
+        }
+    }
+    
+    function reset(){
+        clearInterval(timer);
+        startTime = 0;
+        elapsedTime = 0;
+        isRunning = false;
+        display.textContent = "00:00:00.00";
+    }
+    
+    function update(){
+        const currentTime = Date.now();
+        elapsedTime = currentTime - startTime;
+        
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / 1000 % 60 );
+        let milliseconds = Math.floor(elapsedTime % 1000 / 10 );
+        
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
+        
+        display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+    }
+    
+}
+
+//stopwatch
